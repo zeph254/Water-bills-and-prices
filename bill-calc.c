c2d447427ede9d394a1e3994b2eabaa83c155d98
zeph254 <ularezephaniah@gmail.com>
2025-10-02 10:39:53 +0300

    price-calculation

---
+++ bill-calc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int units;
+    float bill = 0.0;
+
+    // Input units consumed
+    printf("Enter water units consumed: ");
+    scanf("%d", &units);
+
+    // Calculate bill using if-else conditions
+    if (units <= 30) {
+        bill = units * 20.0;
+    } else if (units <= 60) {
+        bill = (30 * 20.0) + ((units - 30) * 25.0);
+    } else {
+        bill = (30 * 20.0) + (30 * 25.0) + ((units - 60) * 30.0);
+    }
+
+    // Display total bill with 2 decimal places
+    printf("Total water bill: %.2f KES\n", bill);
+
+    return 0;
+}
